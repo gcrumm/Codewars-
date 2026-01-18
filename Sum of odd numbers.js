f5ae9537717f5acd5ec7c7b8795b9916f60d9990
Gary Crumm <garycrumm@yahoo.com>
2026-01-18 20:32:47 +0900

    Added a new codwars challenge

---
+++ Sum of odd numbers.js
@@ -0,0 +1,25 @@
+// Description:
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+// My solution
+
+function rowSumOddNumbers(n) {
+  let sum = 0
+	let firstNum = n * (n-1) + 1
+  for (let i = 0; i < n; i++){
+    sum += firstNum
+    firstNum += 2
+  }
+  return sum
+}
