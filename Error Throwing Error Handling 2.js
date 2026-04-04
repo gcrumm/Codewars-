750d4514fc0c207256d46f2ac8b06070aed2806f
Gary Crumm <garycrumm@yahoo.com>
2026-04-04 19:51:42 +0900

    Added a new codewars challenge

---
+++ Error Throwing Error Handling 2.js
@@ -0,0 +1,35 @@
+// Description:
+// Error Throwing - Error Handling #2
+// Error Handling is very important in coding. Most error handling seems to be overlooked or not implemented properly.
+
+// Task
+// In this kata you are provided to evaluate a string, you must check for any HTML code (i.e. if any HTML tags are found), if any code is found you must return false, If the input is not a string you must throw a TypeError, if the string is over 255 characters long or contains 0 characters you must throw a RangeError and last of all if the string entered is null throw a ReferenceError.
+
+// Error Messages
+// ReferenceError
+
+// new ReferenceError('Message is null!')
+// TypeError
+
+// new TypeError(`Message should be of type string but was of type ${typeof msg}!`)
+// RangeError
+
+// new RangeError(`Message contains ${msg.length} characters!`)
+
+// My solution
+
+function validateMessage(msg) {
+  function hasHtml(str) {
+  return /<[^>]+>/.test(str);
+}
+  if(msg === null){
+    throw new ReferenceError('Message is null!');
+  }else if(typeof msg !== 'string'){
+    throw new TypeError(`Message should be of type string but was of type ${typeof msg}!`);
+  }else if(msg.length === 0 || msg.length > 255){
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+  }else if(hasHtml(msg)){
+    return false;
+  }
+  return true;
+  }
