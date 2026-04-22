80d74826db7bca89c66df552d6801323ba370c6d
Gary Crumm <garycrumm@yahoo.com>
2026-04-22 21:41:00 +0900

    Added a new codewars challenge

---
+++ Area of a square.js
@@ -0,0 +1,11 @@
+// Description:
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input.
+
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+// My solution
+
+function squareArea(A) {
+    let area = (4 * A ** 2) / (Math.PI ** 2);
+    return area
+}
