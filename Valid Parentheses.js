40b05af1253cfc9434fe2fa69f4aa1621e7edc3c
Gary Crumm <garycrumm@yahoo.com>
2026-05-31 12:54:18 +0900

    Added a new codewars challenge

---
+++ Valid Parentheses.js
@@ -0,0 +1,31 @@
+// Description:
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+// Constraints
+// 0 <= length of input <= 100
+
+// All inputs will be strings, consisting only of characters ( and ).
+// Empty strings are considered balanced (and therefore valid), and will be tested.
+// For languages with mutable strings, the inputs should not be mutated.
+
+// Protip: If you are trying to figure out why a string of parentheses is invalid, paste the parentheses into the code editor, and let the code highlighting show you!
+
+// My solution
+
+function validParentheses(parenStr) {
+  let total = 0;
+  for(let char of parenStr){
+    if(char === '('){
+      total ++;
+    }else if(char === ')') {
+      total --;
+    }
+    if(total < 0) return false;
+    }
+   return total === 0;
+  }
