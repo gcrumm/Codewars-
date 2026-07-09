8d7116ab57ae9ebe9dd3eefc21106178ef6087d8
Gary Crumm <garycrumm@yahoo.com>
2026-07-09 16:40:08 +0900

    Added a new codewars challenge

---
+++ Clean up after your dog.js
@@ -0,0 +1,34 @@
+// Description:
+// You have stumbled across the divine pleasure that is owning a dog and a garden. Now time to pick up all the cr@p! :D
+
+// Given a 2D array to represent your garden, you must find and collect all of the dog cr@p - represented by '@'.
+
+// You will also be given the number of bags you have access to (bags), and the capactity of a bag (cap). If there are no bags then you can't pick anything up, so you can ignore cap.
+
+// You need to find out if you have enough capacity to collect all the cr@p and make your garden clean again.
+
+// If you do, return 'Clean', else return 'Cr@p'.
+
+// Watch out though - if your dog is out there ('D'), he gets very touchy about being watched. If he is there you need to return 'Dog!!'.
+
+// For example:
+
+// bags = 2
+// cap = 2
+// x (or garden) =
+// [[ _ , _ , _ , _ , _ , _ ],
+//  [ _ , _ , _ , _ , @ , _ ],
+//  [ @ , _ , _ , _ , _ , _ ]]
+
+//  My solution
+
+function crap(garden, bags, cap){
+  const limit = bags * cap;
+  let count = 0;
+  garden = garden.flat()
+  if(garden.includes('D')) return 'Dog!!';
+  for(let el of garden){
+    if(el === '@') count ++ ;
+  }
+   return count > limit ? 'Cr@p' : 'Clean';
+};
