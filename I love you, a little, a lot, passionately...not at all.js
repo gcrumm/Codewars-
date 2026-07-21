6304d818632262f66d795ec293f760916a99eb8c
Gary Crumm <garycrumm@yahoo.com>
2026-07-21 23:43:07 +0900

    Added a new codewars challenge

---
+++ I love you, a little, a lot, passionately...not at all.js
@@ -0,0 +1,26 @@
+// Description:
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// 1. "I love you"
+// 2. "a little"
+// 3. "a lot"
+// 4. "passionately"
+// 5. "madly"
+// 6. "not at all"
+// If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+// My solution
+
+function howMuchILoveYou(nbPetals) {
+    let phrase = nbPetals % 6;
+    if(phrase === 1) return "I love you";
+    if(phrase === 2) return "a little";
+    if(phrase === 3) return "a lot";
+    if(phrase === 4) return "passionately";
+    if(phrase === 5) return "madly";
+    if(phrase === 0) return "not at all";
+  }
