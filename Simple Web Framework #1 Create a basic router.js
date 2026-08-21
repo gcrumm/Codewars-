1a038c300b81337c5a1d91b687405e6bb823ff7a
Gary Crumm <garycrumm@yahoo.com>
2026-08-21 13:04:07 +0900

    Added a new codewars challenge

---
+++ Simple Web Framework #1 Create a basic router.js
@@ -0,0 +1,32 @@
+// Description:
+// In this Kata, you have to design a simple routing class for a web framework.
+
+// The router should accept bindings for a given url, http method and an action.
+
+// Then, when a request with a bound url and method comes in, it should return the result of the action.
+
+// Example usage:
+
+// var router = new Router;
+// router.bind('/hello', 'GET', function(){ return 'hello world'; });
+
+// router.runRequest('/hello', 'GET') // returns 'hello world';
+// When asked for a route that doesn't exist, router should return:
+
+// 'Error 404: Not Found'
+// The router should also handle modifying existing routes. See the example tests for more details.
+
+// My solution
+
+let Router = function(){
+  this.routes = {};
+};
+
+Router.prototype.bind = function(url, method, action){
+  this.routes[url + method] = action;
+};
+
+Router.prototype.runRequest = function(url, method){
+  const action = this.routes[url + method];
+  return action ? action() : 'Error 404: Not Found';
+};
