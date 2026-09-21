596ee45d82d87f1d753b4530885fae331e1028f2
Gary Crumm <garycrumm@yahoo.com>
2026-09-21 13:24:45 +0900

    Added a new codewars challenge

---
+++ Small enough-Beginner.js
@@ -0,0 +1,15 @@
+// Description:
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+// My solution
+
+function smallEnough(a, limit){
+  let count = 0;
+  for(let num of a){
+    if(num <= limit) 
+      count ++;
+  }
+ return count === a.length;
+}
